a77c5167b3316f3a72c91b6611f5460ca104b1ce
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 12:23:41 -0400

    Created footer file.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="h-[5vh] border-2 border-black flex justify-center items-center">
+            <p className="text-md">&copy; Copyright By Billy English</p>
+        </footer>
+    );
+};
+
+export default Footer;
