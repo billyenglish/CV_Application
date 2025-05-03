6b91b2fd58d9d7498becb7dc9ebd62ba34b3b605
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 17:32:32 -0400

    Updated Nav with decoy labels.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+const Nav = () => {
+
+    return (
+        <nav>
+            <div>
+                <ul>
+                    <li>Personal Details</li>
+                    <li>Links</li>
+                    <li>Technical Skills</li>
+                    <li>Work Experience</li>
+                    <li>Projects</li>
+                    <li>Education</li>
+                    <li>Certification, Skills</li>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li>Live Preview</li>
+                    <li>Preview PDF & Download</li>
+                    <li>Clear Form Data</li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
