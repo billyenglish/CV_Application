df325a1a26424528137ffa89783aadeae1ea7c4c
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 12:28:54 -0400

    Adding Home component.

---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import Footer from "../components/Footer";
+
+const Home = () => {
+
+    return (
+        <>
+        <section className="flex items-center justify-center border-1 min-w-screen h-[95vh]">
+            <div className="flex flex-col gap-10">
+                    <h1 className="text-6xl text-extralight text-shadow-orange-500">Cover Letter Builder</h1>
+                    <div className="flex justify-center">
+                        <button
+                            type="button"
+                            className="uppercase
+                                text-xl border-2 pt-1
+                                pb-1 pl-2 pr-2 rounded-lg
+                                cursor-pointer
+                                hover:animate-pulse
+                                hover:text-white
+                                hover:bg-black
+                            "
+                            onClick={() => "Hello World"}
+                        >
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
