08a7513cb273b914ce47e8035888a84a3cdc23a6
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 17:34:42 -0400

    Updated the button component form for prop drilling.

---
+++ src/subcomponents/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({text}) => {
+
+    return (
+        <>
+            <button
+                className="text-lg
+                 border-2 border-black w-30">{text}</button>
+        </>
+    );
+};
+
+export default Button;
