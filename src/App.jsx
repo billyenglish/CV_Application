b3f8d0b1f70bc6f6514a9f4c8bb212869b7faf95
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 13:42:44 -0400

    Created Personal Detail Section.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import PersonalDetails from "./components/PersonalDetails";
+
+function App() {
+
+  return (
+    <>
+      <PersonalDetails />
+    </>
+  )
+}
+
+export default App
