62d036567a013b62bb6541a95767bc9c63eb6226
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 21:56:26 -0400

    Centered UI layout form header button components.

--- src/components/SocialLinkDetails.jsx
+++ src/components/SocialLinkDetails.jsx
@@ -8,54 +8,57 @@ const SocialLinkDetails = () => {
         <>
             <div className="flex h-[95vh] min-w-full gap-2">
                 <Nav />
-                <section className="flex flex-col w-[95%]">
-                    <div className="flex">
+                <section className="flex flex-col items-center w-[95%]">
+                    <div className="flex items-center content-center gap-20 pt-16 pb-18">
                         <h2 className="text-3xl text-light tracking-wider">Social Links</h2>
-                        <div>
+                        <div className="flex gap-2">
                             <Button type="button" text="Back" />
                             <Button type="button" text="Next" />
                         </div>
                     </div>
 
-                    <form className="flex flex-col border-2 border-black">
-                        <div className="flex items-center justify-evenly border-black">
+                    <form className="flex flex-col gap-10 p-10 pb-36 border-2 border-black rounded-sm">
+                        <div className="flex items-center justify-evenly gap-10 border-black">
                             <label>Website</label>
                             <UserInput
                                 type="text"
                                 placeholder="Website Link"
-                                className="h-12 border-2 border-black"
+                                className="h-12 w-72 border-2 border-black"
                             />
 
                             <UserInput
                                 type="text"
                                 placeholder="UserName"
-                                className="h-12 border-2 border-black"
+                                className="h-12 w-72 border-2 border-black"
                             />
                         </div>
 
-                        <div className="flex items-center justify-evenly border-black">
+                        <div className="flex items-center justify-evenly gap-10 border-black">
                             <label>Linkedin</label>
                             <UserInput
                                 type="text"
                                 placeholder="LinkedIn Link"
-                                className="h-12 border-2 border-black"
+                                className="h-12 w-72 border-2 border-black"
                             />
 
                             <UserInput
                                 type="text"
                                 placeholder="LinkedIn Username"
+                                className="h-12 w-72 border-2 border-black"
                             />
                         </div>
-                        <div>
+                        <div className="flex items-center justify-evenly gap-10 border-black">
                             <label>GitHub</label>
                             <UserInput
                                 type="text"
                                 placeholder="GitHub Link"
+                                className="h-12 w-72 ml-2.5 border-2 border-black"
                             />
 
                             <UserInput
                                 type="text"
                                 placeholder="GitHub Link"
+                                className="h-12 w-72 border-2 border-black"
                             />
                         </div>
                     </form>
