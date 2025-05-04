57c9d61a315bd9805654eb000dd5bee2c991f76c
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 21:53:47 -0400

    Doing testing for UI layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
 import PersonalDetails from "./components/PersonalDetails";
+import SocialLinkDetails from "./components/SocialLinkDetails";
+import Footer from "./components/Footer";
 
 function App() {
 
