85a10b12a8b90563b8ec9a9e5e7e6caed562842a
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 22:17:03 -0400

    Created WorkDetail component.

---
+++ src/components/WorkDetails.jsx
@@ -0,0 +1,18 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+const WorkDetails = () => {
+    return (
+        <>
+            <div>
+                <Nav />
+                <section>
+                    Work Details
+                </section>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default WorkDetails;
