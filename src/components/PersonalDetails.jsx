921e05e45a1a3ea01b841cebf696afd7778199cd
Demar English <demarenglish@Mac.attlocal.net>
2025-05-03 20:00:44 -0400

    Updated the Person Details UI removing any gaps.

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -9,10 +9,22 @@ const PersonalDetails = () => {
         <>
             <div className="h-[95vh] min-w-full flex gap-2">
                 <Nav className="border-2 border-black" />
-                <section className="flex flex-col items-center border-2 border-black min-w-[90%]">
+                <section className="flex flex-col items-center border-2 border-black w-[95%]">
 
+                    <div className="flex items-center content-center gap-20 pt-16 pb-18">
+                        <h2 className="text-3xl font-light tracking-wider">Personal Details</h2>
+                        <div className="flex self-center items-center gap-10 h-full">
+                            <Button
+                                type="button"
+                                text="Back"
+                            />
 
-                    <h2 className="text-3xl font-light tracking-wider pt-12 pb-20">Personal Details</h2>
+                            <Button
+                                type="button"
+                                text="Next"
+                            />
+                        </div>
+                    </div>
 
                     <form className="flex flex-col gap-4 border-2 border-black p-6 rounded-md">
                         <div className="flex gap-6">
@@ -77,8 +89,11 @@ const PersonalDetails = () => {
                                     border-2
                                     border-black
                                     h-64 w-150
+                                    resize-none
+                                    overflow-y-hidden
                                     rounded-sm
                                 "
+                                placeholder="Summary "
                                 >
                             </textarea>
                         </div>
