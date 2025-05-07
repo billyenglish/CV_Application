b901327b0ef39e8bfbd1196e726be2811e4a1557
Billy English <billyenglish6212@outlook.com>
2025-05-07 19:17:00 -0400

    Updated the layout for the input boxes.

--- src/components/SocialLinkDetails.jsx
+++ src/components/SocialLinkDetails.jsx
@@ -1,69 +1,83 @@
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
+import Nav from "./Nav";
+import Footer from "./Footer";
 import Button from "../subcomponents/Button";
 import UserInput from "../subcomponents/UserInput";
 
 const SocialLinkDetails = () => {
+
     return (
         <>
-            <div className="flex h-[95vh] min-w-full gap-2">
-                <Nav />
-                <section className="flex flex-col items-center w-[95%]">
-                    <div className="flex items-center content-center gap-20 pt-16 pb-18">
-                        <h2 className="text-3xl text-light tracking-wider">Social Links</h2>
-                        <div className="flex gap-10 h-full">
-                            <Button type="button" text="Back" />
-                            <Button type="button" text="Next" />
+            <Nav />
+            <section className="flex flex-col items-center border-2 min-h-screen gap-40">
+                <div className="flex justify-center items-center pt-20 gap-14">
+                    <h2 className="text-3xl font-light">Social Links</h2>
+                    <div className="flex gap-6">                    
+                        <Button text="Back" />
+                        <Button text="Next" />
+                    </div>
+                </div>
+                <form className="flex flex-col border-2 w-auto gap-10 items-center pl-6 pr-6 pt-10 pb-20 rounded-md ">
+                    <div className="flex flex-row items-end gap-6">
+                        <h3 className="text-lg mb-1">Portfolio Website</h3>
+                        <div className="flex gap-10">
+                            <div className="flex flex-col gap-2">
+                                <label className="text-xl font-light">Website Link</label>
+                                <UserInput
+                                    type="text"
+                                    placeholder="Full Name"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <label className="text-xl font-light">Website Description</label>
+                                <UserInput
+                                    type="text"
+                                    placeholder="Job Title"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
                         </div>
                     </div>
-
-                    <form className="flex flex-col gap-10 p-10 pb-36 border-2 border-black rounded-sm">
-                        <div className="flex items-center justify-evenly gap-10 border-black">
-                            <label className="text-xl font-normal">Website</label>
-                            <UserInput
-                                type="text"
-                                placeholder="Website Link"
-                                className="h-12 w-72 border-2 border-black"
-                            />
-
-                            <UserInput
-                                type="text"
-                                placeholder="UserName"
-                                className="h-12 w-72 border-2 border-black"
-                            />
+                    <div className="flex flex-row justify-evenly items-end gap-6">
+                        <h3 className="text-lg mb-1">LinkedIn</h3>
+                        <div className="flex gap-10">
+                            <div className="flex flex-col gap-2 ml-20">
+                                <UserInput
+                                    type="text"
+                                    placeholder="Full Name"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <UserInput
+                                    type="text"
+                                    placeholder="Job Title"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
                         </div>
-
-                        <div className="flex items-center justify-evenly gap-10 border-black">
-                            <label className="text-xl font-normal">Linkedin</label>
-                            <UserInput
-                                type="text"
-                                placeholder="LinkedIn Link"
-                                className="h-12 w-72 border-2 border-black"
-                            />
-
-                            <UserInput
-                                type="text"
-                                placeholder="LinkedIn Username"
-                                className="h-12 w-72 border-2 border-black"
-                            />
+                    </div>
+                    <div className="flex flex-row justify-evenly items-end gap-6">
+                        <h3 className="text-lg mb-1">GitHub</h3>
+                        <div className="flex gap-10">
+                            <div className="flex flex-col gap-2 ml-22">
+                                <UserInput
+                                    type="text"
+                                    placeholder="Full Name"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <UserInput
+                                    type="text"
+                                    placeholder="Job Title"
+                                    className="border-2 h-10 w-70"
+                                />
+                            </div>
                         </div>
-                        <div className="flex items-center justify-evenly gap-10 border-black">
-                            <label className="text-xl font-normal">GitHub</label>
-                            <UserInput
-                                type="text"
-                                placeholder="GitHub Link"
-                                className="h-12 w-72 ml-2.5 border-2 border-black"
-                            />
-
-                            <UserInput
-                                type="text"
-                                placeholder="GitHub Link"
-                                className="h-12 w-72 border-2 border-black"
-                            />
-                        </div>
-                    </form>
-                </section>
-            </div>
+                    </div>
+                </form>
+            </section>
             <Footer />
         </>
     );
