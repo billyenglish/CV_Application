13a919ed757647316d2199ae973b68bb206f0cca
Demar English <demarenglish@Mac.attlocal.net>
2025-05-08 01:26:57 -0400

    Updated parameters for the type of button.

--- src/subcomponents/Button.jsx
+++ src/subcomponents/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({text}) => {
+const Button = ({type, text, className = "text-xl font-light hover:cursor-pointer hover:scale-110 border-2 border-black w-30 rounded-sm"}) => {
 
     return (
         <>
             <button
-                className="text-xl font-light hover:cursor-pointer hover:scale-110
-                 border-2 border-black w-30 rounded-sm"
+                type={type}
+                className={className}
             >{text}
             </button>
         </>
