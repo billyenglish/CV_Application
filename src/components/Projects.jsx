fbcc70a3c4165cbbaff2546f2609cde735d0b218
Demar English <demarenglish@Mac.attlocal.net>
2025-05-07 21:32:18 -0400

    Updated the section title and added button components for back and next buttons.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,22 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Button from "../subcomponents/Button";
+
 const Projects = () => {
+
     return (
         <>
-            Projects
+            <Nav />
+            <section className="flex flex-col items-center border-2 min-h-screen gap-32">
+                <div className="flex justify-center items-center pt-14 gap-14">
+                    <h2 className="text-3xl font-light">Projects</h2>
+                    <div className="flex gap-6">
+                        <Button text="Back" />
+                        <Button text="Next" />
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     );
 };
