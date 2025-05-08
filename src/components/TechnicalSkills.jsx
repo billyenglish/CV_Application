b07cc39c2250affd14d73d8258509fc3f01613e8
Demar English <demarenglish@Mac.attlocal.net>
2025-05-08 01:28:25 -0400

    Created input box in a unordered list.

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -1,8 +1,10 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 import Button from "../subcomponents/Button";
+import UserInput from "../subcomponents/UserInput";
 
 const TechnicalSkills = () => {
+
     return (
         <>
             <Nav />
@@ -14,6 +16,29 @@ const TechnicalSkills = () => {
                         <Button text="Next" />
                     </div>
                 </div>
+                <form className="flex flex-col">
+                    <h3 className="text-3xl font-light tracking pb-10 text-center">Add Skills</h3>
+                    <div>
+                        <ul className="flex flex-col gap-6">
+                            <li className="flex gap-2">
+                                <UserInput type="text" className="border-2 h-10 w-80" placeholder="Skill" />
+                                <Button type="button" text="Remove" className="border-2 h-10 pl-2 pr-2 rounded-sm" />
+                            </li>
+                            <li className="flex gap-2">
+                                <UserInput type="text" className="border-2 h-10 w-80" placeholder="Skill" />
+                                <Button type="button" text="Remove" className="border-2 h-10 pl-2 pr-2 rounded-sm" />
+                            </li>
+                            <li className="flex gap-2">
+                                <UserInput type="text" className="border-2 h-10 w-80" placeholder="Skill" />
+                                <Button type="button" text="Remove" className="border-2 h-10 pl-2 pr-2 rounded-sm" />
+                            </li>
+                            <li className="flex gap-2">
+                                <UserInput type="text" className="border-2 h-10 w-80" placeholder="Skill" />
+                                <Button type="button" text="Remove" className="border-2 h-10 pl-2 pr-2 rounded-sm" />
+                            </li>
+                        </ul>
+                    </div>
+                </form>
             </section>
             <Footer />
         </>
