d160de3592f4442b867d5c3089a12b676d3baa36
Demar English <demarenglish@Mac.attlocal.net>
2025-05-07 21:34:14 -0400

    Updated the section title and added button components for back and next buttons.

--- src/components/WorkDetails.jsx
+++ src/components/WorkDetails.jsx
@@ -1,9 +1,24 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Button from "../subcomponents/Button";
+
 const WorkDetails = () => {
+
     return (
         <>
-            Work Details
+            <Nav />
+            <section className="flex flex-col items-center border-2 min-h-screen gap-32">
+                <div className="flex justify-center items-center pt-14 gap-14">
+                    <h2 className="text-3xl font-light">Work Details</h2>
+                    <div className="flex gap-6">
+                        <Button text="Back" />
+                        <Button text="Next" />
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     );
-};
+}
 
 export default WorkDetails;
