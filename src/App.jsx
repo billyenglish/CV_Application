d82b45c2094d1485a25d21c42282efc24f7e088c
Demar English <demarenglish@Mac.attlocal.net>
2025-07-03 23:02:04 -0400

    Wiped index.css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import PersonalDetails from "./components/PersonalDetails";
-import SocialLinkDetails from "./components/SocialLinkDetails";
-import WorkDetails from "./components/WorkDetails";
-import Education from "./components/Education";
-import TechnicalSkills from "./components/TechnicalSkills";
-import Projects from "./components/Projects";
-import CertsSkillsInterests from "./components/CertsSkillsInterests";
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/personal-details" element={<PersonalDetails />} />
-        <Route path="/social-link-detail" element={<SocialLinkDetails />} />
-        <Route path="/work-detail" element={<WorkDetails />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/technical-skills" element={<TechnicalSkills />} />
-        <Route path="/project" element={<Projects />} />
-        <Route path="/certification" element={<CertsSkillsInterests />} />
-      </Routes>
-    </BrowserRouter>
+    <section>
+      
+    </section>
   );
 }
 
