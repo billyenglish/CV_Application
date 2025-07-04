eee0585efa23569f42f2843ecbfc633daad2fe40
Demar English <demarenglish@Mac.attlocal.net>
2025-07-03 22:55:36 -0400

    Wiped Project.

--- src/subcomponents/TextArea.jsx
+++
@@ -1,8 +0,0 @@
-const TextArea = (props) => {
-    return (
-        <textarea className={props.className} placeholder={props.placeholder}>
-        </textarea>
-    );
-}
-
-export default TextArea;
