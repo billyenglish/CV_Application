951a7c7af49012c584809c8552984c4c6ba6a8a2
Demar English <demarenglish@Mac.attlocal.net>
2025-08-17 12:49:47 -0400

    Create Homepage css structure.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,23 @@
+import '../styles/homepage.css'
+import Button from "../subcomponents/Button";
+import { useNavigate } from 'react-router-dom';
+
 const Homepage = () => {
+
+    const currentNav = useNavigate('')
+
     return (
-        <section>
-            
+        <section className="homepage_section">
+            <div className="homepage_content">
+                <h1 className='homepage_title'>Cover Letter Builder</h1>
+                <div>
+                    <Button
+                        text="Get Started"
+                        className="get_started"
+                        onClick={() => currentNav('personal-information')}
+                    />
+                </div>
+            </div>
         </section>
     );
 };
