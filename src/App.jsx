67c39e00942b884ba0bce6dabb4123046c70af38
Demar English <demarenglish@Mac.attlocal.net>
2025-08-17 12:49:09 -0400

    Creating routing to each component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import { Routes, Route } from "react-router-dom"
+import Homepage from "./components/Homepage"
+import PersonalInformation from "./components/PersonalInformation"
+import WorkExperience from "./components/WorkExperience"
+import Education from "./components/Education"
+import Links from "./components/Links"
+import Projects from "./components/Projects"
 
 function App() {
 
   return (
-    <section>
-      
-    </section>
+    <>
+      <Routes>
+        <Route index="/" element={<Homepage />} />
+        <Route path="personal-information" element={<PersonalInformation />} />
+        <Route path="work-experience" element={<WorkExperience />} />
+        <Route path="education" element={<Education />} />
+        <Route path="links" element={<Links />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </>
   );
 }
 
